d6cbed2b0127ab94d9cd3c0648b2952b4724f424
trycatch <sgykim36@gmail.com>
2024-11-29 14:52:36 +0900

    Game framework - GameInstance / GameMode

---
+++ S1/Source/S1/System/S1GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "S1GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S1_API US1GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
