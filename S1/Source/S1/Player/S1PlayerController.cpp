2bab4d9b425a398264e7479a496ebcdefd86cf39
trycatch <sgykim36@gmail.com>
2024-12-01 15:48:45 +0900

    EnhancedInputComponent / MappingContext

--- S1/Source/S1/Player/S1PlayerController.cpp
+++ S1/Source/S1/Player/S1PlayerController.cpp
@@ -2,4 +2,69 @@
 
 
 #include "S1PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/KismetMathLibrary.h"
 
+AS1PlayerController::AS1PlayerController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void AS1PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (auto* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(InputMappingContext, 0);	// InputMappingContext, priority
+	}
+}
+
+void AS1PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (auto* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(TestAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Test);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Turn);
+	}
+}
+
+void AS1PlayerController::Input_Test(const FInputActionValue& InputValue)
+{
+	GEngine->AddOnScreenDebugMessage(0, 1.0f, FColor::Cyan, TEXT("Input_Test"));
+}
+
+void AS1PlayerController::Input_Move(const FInputActionValue& InputValue)
+{
+	FVector2D MovementVector = InputValue.Get<FVector2D>();
+
+	if (MovementVector.X != 0)
+	{
+		//FVector Direction = FVector::ForwardVector * MovementVector.X;
+		//GetPawn()->AddActorWorldOffset(Direction * 50.f);
+
+		FRotator Rotator = GetControlRotation();
+		FVector Direction = UKismetMathLibrary::GetForwardVector(FRotator(0, Rotator.Yaw, 0));
+		GetPawn()->AddMovementInput(Direction, MovementVector.X);
+	}
+
+	if (MovementVector.Y != 0)
+	{
+		//FVector Direction = FVector::RightVector * MovementVector.Y;
+		//GetPawn()->AddActorWorldOffset(Direction * 50.f);
+		
+		FRotator Rotator = GetControlRotation();
+		FVector Direction = UKismetMathLibrary::GetRightVector(FRotator(0, Rotator.Yaw, 0));
+		GetPawn()->AddMovementInput(Direction, MovementVector.Y);
+	}
+}
+
+void AS1PlayerController::Input_Turn(const FInputActionValue& InputValue)
+{
+	float val = InputValue.Get<float>();
+	AddYawInput(val);	// ** Apply at PlayerController
+}
